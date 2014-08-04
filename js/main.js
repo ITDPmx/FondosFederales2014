cf245b733dc697be1654bcfc10d72accd7345a44
Mario Flores <pokazzam@gmail.com>
2014-08-03 20:52:07 -0500

    se elimino main.js, archivo innecesario

--- js/main.js
+++ js/main.js
@@ -1,18 +1,4 @@
-(function(){
-	$('#acerca').modal();
-	
-});    
+
 
 /*	var language = window.navigator.userLanguage || navigator.language;
 	console.log(language);*/
-
-/*L.DomEvent.addListener(estado, 'change', function (e) {
-	var valores = JSON.parse(e.target.value);
-	console.log(valores);
-	for (var i in valores) {
-		console.log(i+valores[i]);
-		
-	};
-	info.update(e);
-	L.DomEvent.stopPropagation(e);
-});*/
